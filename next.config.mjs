df8a01425934fd20330b2e0517f82421a20faf3a
Sota Tsunemine <motarou.engineer@gmail.com>
2024-06-14 13:35:12 +0900

    add rewrites in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/api/:path*',
+      },
+      {
+        source: '/api/firebase/:path*',
+        destination: '/api/firebase/:path*',
+      },
+    ];
+  },
+};
 
 export default nextConfig;
