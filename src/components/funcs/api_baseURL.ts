6d1f68674b6d669a11be10c02e647e10752b5bff
Sota Tsunemine <motarou.engineer@gmail.com>
2024-06-14 13:43:11 +0900

    change get url system

--- src/components/funcs/api_baseURL.ts
+++ src/components/funcs/api_baseURL.ts
@@ -1,12 +1,24 @@
-const url = (): string => {
-  const vercelUrl = process.env.NEXT_PUBLIC_VERCEL_URL;
-  const netlifyUrl = process.env.NETLIFY_SITE_URL;
+export default function url() {
+  const environment = process.env.NEXT_PUBLIC_VERCEL_ENV;
+  const isProduction = environment === 'production';
+  const isStaging = environment === 'staging';
+  const protocol = isProduction || isStaging ? 'https' : 'http';
 
-  return vercelUrl !== null && vercelUrl !== undefined
-    ? `https://${vercelUrl}`
-    : netlifyUrl !== null && netlifyUrl !== undefined
-      ? `https://${netlifyUrl}`
-      : 'http://localhost:3000';
-};
+  const domain =
+    process.env.NEXT_PUBLIC_VERCEL_URL != null
+      ? process.env.NEXT_PUBLIC_VERCEL_URL
+      : 'localhost:3000';
 
-export default url;
+  if (!isProduction && domain === 'localhost:3000') {
+    return `${protocol}://${domain}`;
+  }
+
+  const urlObject = new URL(`${protocol}://${domain}`);
+  const hostnameSplit = urlObject.hostname.split('.');
+  const subdomain = hostnameSplit[0] != null ? hostnameSplit[0].split('-')[0] : '';
+  const secondLevelDomain = hostnameSplit[1];
+  const topLevelDomain = hostnameSplit[2];
+
+  const finalDomain = `${subdomain}.${secondLevelDomain}.${topLevelDomain}`;
+  return `${protocol}://${finalDomain}`;
+}
